5e56f1aa70b4d46bb4d862365bbbff7741840e0d
romaric roussel <romaric.roussel@outlook.fr>
2018-12-05 17:04:57 +0100

    jeux qui marche no css

--- src/index.js
+++ src/index.js
@@ -12,43 +12,45 @@ class Game extends React.Component {
     let playerX = prompt("Nom joueur X");
     let playerO = prompt("Nom joueur O");
     let celluleRestante = parseInt(nbLigneEtColonne) * parseInt(nbLigneEtColonne);
-    
-    
+
+
 
     this.state = {
       tableaux: this.createTableau(nbLigneEtColonne),
       currentTableau: this.createTableau(nbLigneEtColonne),
-      lastIndexI:"",
-      lastIndexJ:"",
+      lastIndexI: "",
+      lastIndexJ: "",
       xIsNext: true,
       nbLigne: parseInt(nbLigneEtColonne),
       nbColonne: parseInt(nbLigneEtColonne),
-      nbLigneEtColonne:parseInt(nbLigneEtColonne),
+      nbLigneEtColonne: parseInt(nbLigneEtColonne),
       currentSymbole: "",
       scoreJoueurX: 0,
       scoreJoueurO: 0,
       playerX: playerX,
       playerO: playerO,
       partiFini: false,
-      matchNul:false,
+      matchNul: false,
       celluleRestante: celluleRestante,
       status: "",
       move: 0,
-      winner:""
+      winner: ""
 
 
     };
+
+
   }
- createTableau(nbLigneEtColonne){
-   let tab = [];
-   for(let i=0;i<nbLigneEtColonne;i++){
-     tab[i] = [];
-     for(let j=0;j<nbLigneEtColonne;j++){
-      tab[i][j] = "";
+  createTableau(nbLigneEtColonne) {
+    let tab = [];
+    for (let i = 0; i < nbLigneEtColonne; i++) {
+      tab[i] = [];
+      for (let j = 0; j < nbLigneEtColonne; j++) {
+        tab[i][j] = "";
+      }
     }
-   }
-   return tab;
- }
+    return tab;
+  }
 
   victoireLigne(symbol) {
 
@@ -130,87 +132,96 @@ class Game extends React.Component {
   }
   handleClick(i, j) {
 
-    
-    if (this.state.partiFini===false) {
-      if (!this.caseVide(i, j) && this.state.celluleRestante !== 0 ) {
+
+    if (this.state.partiFini === false) {
+      if (!this.caseVide(i, j) && this.state.celluleRestante !== 0) {
         alert("Case Pleine");
         return;
       }
-      if (this.state.celluleRestante !== 0){
-       
-        let status = this.state.status;
+      if (this.state.celluleRestante !== 0) {
+        console.log('toto')
         const tableaux = this.state.tableaux.slice();
-        
+        let status = this.state.status;
         tableaux[i][j] = this.state.xIsNext ? 'X' : 'O';
         let symbole = !this.state.xIsNext ? 'X' : 'O';
-        if(symbole === "X"){
+        if (symbole === "X") {
           status = "Tour du joueur : " + this.state.playerX;
         }
-        if(symbole === "O"){
+        if (symbole === "O") {
           status = "Tour du joueur : " + this.state.playerO;
         }
-
-        
-        this.setState({
-          tableaux: tableaux,
-          xIsNext: !this.state.xIsNext,
-          currentSymbole: tableaux[i][j],
-          status: status,
-          celluleRestante : this.state.celluleRestante -1,
-          currentTableau:tableaux,
-          lastIndexI: i,
-          lastIndexJ:j
-          
-
-        });
-      }
-      if (this.state.celluleRestante === 1){
        
-        let status = this.state.status;
-        const tableaux = this.state.tableaux.slice();
-        tableaux[i][j] = this.state.xIsNext ? 'X' : 'O';
-      
-        //let move = this.state.move + 1;
-
-        status = "Match nul"
         this.setState({
           tableaux: tableaux,
           xIsNext: !this.state.xIsNext,
           currentSymbole: tableaux[i][j],
           status: status,
-          celluleRestante : this.state.celluleRestante -1
-
+          celluleRestante: this.state.celluleRestante - 1,
+          currentTableau: tableaux,
+          lastIndexI: i,
+          lastIndexJ: j
+ 
+ 
         });
-        this.updatePartiFini();
-        return;
-      }
-        
-        //this.clonerTableau();
-
         let winner = this.rechercheGagnant(this.state.tableaux[i][j]);
-        console.log(winner);
-        console.log(this.state.tableaux[i][j]);
+
         if (winner === 'X') {
           this.updateScoreJoueurX();
           this.updatePartiFini();
           this.updateStatusX();
           this.setState({
-            winner:winner
+            winner: winner
           });
-          console.log("X");
-        }
-
-        if (winner === 'O') {
+        } else if (winner === 'O') {
           this.updateScoreJoueurO();
           this.updatePartiFini();
           this.updateStatusO();
           this.setState({
-            winner:winner
+            winner: winner
           });
-          console.log("X");
+        }else{
+
         
-        } 
+
+
         
+       }       
+      }
+      if (this.state.celluleRestante === 1) {
+        console.log('tata')
+        let winner = this.rechercheGagnant(this.state.tableaux[i][j]);
+        let status = this.state.status;
+        if (winner === 'X') {
+          this.updateScoreJoueurX();
+          this.updatePartiFini();
+          this.updateStatusX();
+          this.setState({
+            winner: winner
+          });
+        } else if (winner === 'O') {
+          this.updateScoreJoueurO();
+          this.updatePartiFini();
+          this.updateStatusO();
+          this.setState({
+            winner: winner
+          });
+        } else {
+
+          const tableaux = this.state.tableaux.slice();
+          tableaux[i][j] = this.state.xIsNext ? 'X' : 'O';
+          status = "Match nul"
+          this.setState({
+            tableaux: tableaux,
+            xIsNext: !this.state.xIsNext,
+            currentSymbole: tableaux[i][j],
+            status: status,
+            celluleRestante: this.state.celluleRestante - 1
+
+          });
+          this.updatePartiFini();
+                  return;
+        }
+      }
     } else {
       this.alertPartiFini();
     }
@@ -222,7 +233,7 @@ class Game extends React.Component {
       return true;
     }
   }
- 
+
   tableauPlein() {
     for (let i = 0; i < this.state.nbLigne; i++) {
       for (let j = 0; j < this.state.nbColonne; j++) {
@@ -240,50 +251,50 @@ class Game extends React.Component {
     let tableaux = this.createTableau(this.state.nbLigneEtColonne);
     this.resetTableauEtSatus();
     this.setState({
-      celluleRestante :9,
-      winner:"",
-      tableaux:tableaux
+      celluleRestante: 9,
+      winner: "",
+      tableaux: tableaux
     });
   }
   handleBoutonPrecedent() {
-    const currentTableau = this.state.currentTableau.slice();
+    if(this.state.celluleRestante <9){
+      const currentTableau = this.state.currentTableau.slice();
     currentTableau[this.state.lastIndexI][this.state.lastIndexJ] = "";
     let symbole = !this.state.xIsNext ? 'X' : 'O';
     let status = "";
-    if(symbole === "X"){
+    if (symbole === "X") {
       status = "Tour du joueur : " + this.state.playerX;
     }
-    if(symbole === "O"){
+    if (symbole === "O") {
       status = "Tour du joueur : " + this.state.playerO;
     }
     let partiFini = this.state.partiFini;
     let winner = this.state.winner
-    if(partiFini){
-      if(winner==="X"){
+    if (partiFini) {
+      if (winner === "X") {
         this.setState({
-          scoreJoueurX:this.state.scoreJoueurX-1
+          scoreJoueurX: this.state.scoreJoueurX - 1
         });
       }
-      if(winner==="O"){
+      if (winner === "O") {
         this.setState({
-          scoreJoueurO:this.state.scoreJoueurO-1
+          scoreJoueurO: this.state.scoreJoueurO - 1
         });
       }
       this.setState({
-        partiFini:false
+        partiFini: false
       });
 
-      
+
     }
     this.setState({
       currentTableau: currentTableau,
       xIsNext: !this.state.xIsNext,
-      celluleRestante:this.state.celluleRestante +1,
-      status:status   
+      celluleRestante: this.state.celluleRestante + 1,
+      status: status
     });
-    
 
-
+  }
   }
 
   resetTableauEtSatus() {
@@ -307,7 +318,7 @@ class Game extends React.Component {
   }
 
   updatePartiFini() {
-    
+
     this.setState({
       partiFini: true
     });
@@ -348,9 +359,10 @@ class Game extends React.Component {
       scoreJoueurO: scoreO + 1
     });
   }
+
   render() {
-    
-  
+
+
     return (
 
       <div className="game">
@@ -358,38 +370,41 @@ class Game extends React.Component {
           <p>Tic Tac Bug</p>
           <table border="1px"  >
             <tr>
-              <td className="symbole" onClick={() => this.handleClick(0, 0)}>
-                {this.state.tableaux[0][0]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(0, 1)}>
-                {this.state.tableaux[0][1]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(0, 2)}>
-                {this.state.tableaux[0][2]}
-              </td>
+              <Td onClick={() => this.handleClick(0, 0)}
+                tableau={this.state.tableaux[0][0]}
+              ></Td>
+              <Td onClick={() => this.handleClick(0, 1)}
+                tableau={this.state.tableaux[0][1]}
+              ></Td>
+              <Td onClick={() => this.handleClick(0, 2)}
+                tableau={this.state.tableaux[0][2]}
+              ></Td>
             </tr>
             <tr>
-              <td className="symbole" onClick={() => this.handleClick(1, 0)}>
-                {this.state.tableaux[1][0]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(1, 1)}>
-                {this.state.tableaux[1][1]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(1, 2)}>
-                {this.state.tableaux[1][2]}
-              </td>
+
+              <Td onClick={() => this.handleClick(1, 0)}
+                tableau={this.state.tableaux[1][0]}
+              ></Td>
+              <Td onClick={() => this.handleClick(1, 1)}
+                tableau={this.state.tableaux[1][1]}
+              ></Td>
+              <Td onClick={() => this.handleClick(1, 2)}
+                tableau={this.state.tableaux[1][2]}
+              ></Td>
             </tr>
             <tr>
-              <td className="symbole" onClick={() => this.handleClick(2, 0)}>
-                {this.state.tableaux[2][0]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(2, 1)}>
-                {this.state.tableaux[2][1]}
-              </td>
-              <td className="symbole" onClick={() => this.handleClick(2, 2)}>
-                {this.state.tableaux[2][2]}
-              </td>
+              <Td onClick={() => this.handleClick(2, 0)}
+                tableau={this.state.tableaux[2][0]}
+              ></Td>
+              <Td onClick={() => this.handleClick(2, 1)}
+                tableau={this.state.tableaux[2][1]}
+              ></Td>
+              <Td onClick={() => this.handleClick(2, 2)}
+                tableau={this.state.tableaux[2][2]}
+              ></Td>
             </tr>
+
+
           </table>
         </div>
         <div className="game-info">
@@ -421,6 +436,15 @@ function GameBouton(props) {
       <BoutonActionPrecedente onClickPrecedent={props.onClickPrecedent} />
 
     </div>
+  );
+}
+
+
+function Td(props) {
+  return (
+    <td className="symbole" onClick={props.onClick}>
+      {props.tableau}
+    </td>
   );
 }
 
